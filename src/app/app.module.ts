365a46f7b464eed34981c2ebd868140756df4308
Aashman Thing <aashmanthing1@gmail.com>
2022-02-20 22:19:21 +0545

    pwa added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import {BarcodeScannerLivestreamModule} from "ngx-barcode-scanner";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ToastrModule} from "ngx-toastr";
 import {ZXingScannerModule} from "@zxing/ngx-scanner";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,13 @@ import {ZXingScannerModule} from "@zxing/ngx-scanner";
     BarcodeScannerLivestreamModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    ZXingScannerModule
+    ZXingScannerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
